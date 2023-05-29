0a5a7aab2e06d9fede927b8438a991b9b2fafc9f
micchecka <122084453+micchecka@users.noreply.github.com>
2023-05-29 23:25:02 +0300

    Update CollectionRepository.java
    problem with named parameters

--- src/main/java/com/cinema/collection/service/back/repository/CollectionRepository.java
+++ src/main/java/com/cinema/collection/service/back/repository/CollectionRepository.java
@@ -12,16 +12,24 @@ import java.util.List;
 
 @Repository
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
-
-    @Query("SELECT new com.cinema.collection.service.back.dto.response.CollectionResponseDto(" +
-            "c.id, c.name, c.previewId, (SELECT count(m) FROM CollectionMovie m WHERE m.collection.id = :id )," +
-    " (SELECT count(m) FROM UserCollection m WHERE m.collection.id = :id )) from Collection c where  c.id = :id")
+    
+     @Query("SELECT new com.cinema.collection.service.back.dto.response.CollectionResponseDto(" +
+            "c.id, c.name, c.previewId, (SELECT count(m) FROM CollectionMovie m WHERE m.collection.id = c.id), 0) from Collection c  where c.id = :id")
     CollectionResponseDto getCollectionDtoById(Long id);
 
     @Query("SELECT new com.cinema.collection.service.back.dto.response.CollectionResponseDto(" +
-            "c.id, c.name, c.previewId, (SELECT count(m) FROM CollectionMovie m WHERE m.collection.id = :id )," +
-    "(SELECT count(m) FROM UserCollection m WHERE m.collection.id = :id )) from Collection c ")
+            "c.id, c.name, c.previewId, (SELECT count(m) FROM CollectionMovie m WHERE m.collection.id = c.id), 0) from Collection c")
     List<CollectionResponseDto> getAllCollectionDto();
+
+   // @Query("SELECT new com.cinema.collection.service.back.dto.response.CollectionResponseDto(" +
+//             "c.id, c.name, c.previewId, (SELECT count(m) FROM CollectionMovie m WHERE m.collection.id = :id )," +
+//     " (SELECT count(m) FROM UserCollection m WHERE m.collection.id = :id )) from Collection c where  c.id = :id")
+//     CollectionResponseDto getCollectionDtoById(Long id);
+
+//     @Query("SELECT new com.cinema.collection.service.back.dto.response.CollectionResponseDto(" +
+//             "c.id, c.name, c.previewId, (SELECT count(m) FROM CollectionMovie m WHERE m.collection.id = :id )," +
+//     "(SELECT count(m) FROM UserCollection m WHERE m.collection.id = :id )) from Collection c ")
+//     List<CollectionResponseDto> getAllCollectionDto();
 
     @Query(value = """
             SELECT new com.cinema.collection.service.back.dto.SearchCollectionDto(
